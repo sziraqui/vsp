67a6d0ed0066830619440280679cd60d0f3a7c1c
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2018-11-04 15:08:18 +0530

    functions to prepare labels from transcript

---
+++ modules/textprocessing.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+
+'''
+    Extract word start and end timestamps from a transcript line
+    arg1: A string of format "start_frame end_frame word"
+    returns: normalized word start, end frame and acutual word
+'''
+def extract_timestamps_and_word(transcript_line):
+    wordStart, wordEnd, word = transcript_line.split()
+
+    wordStart = int(np.round(float(wordStart)))
+    wordEnd = int(np.round(float(wordEnd)))
+    return wordStart, wordEnd, word
+
+
+'''
+    Converts word into array of code points in range 0-27 for a-z + space + ctc_blank
+    Helper function for word2bin_seq()
+    agr1: str
+    returns: numpy.array of same len as arg1
+'''
+def word2ints(word):
+    codePoints = np.array(list(map(lambda x: ord(x) - ord('a'), word)))
+    return codePoints
+
+
+'''
+    Convert str word into len(word)*28 size binary matrix
+    ith row has a 1 at column index representing code-point(0-27) of ith letter, rest all 0s
+    arg1: normalized start frame
+    arg2: normalized end frame of word occurence in video
+    arg3: word [a-z] (small caps) as str
+'''
+def word2binmat(wordStart, wordEnd, word):
+    size = wordEnd - wordStart + 1
+    wordBins = np.zeros((size, 28))
+ 
+    if word in ['sp', 'sil']:
+        wordBins[:, 27] = 1 # 27 denotes CTC blank
+        return wordBins
+    codePoints = word2ints(word)
+    # Distribute word over the entire frame width
+    repetitions = size//len(word)
+    lastFill = 0
+    for i in codePoints:
+        wordBins[lastFill:(lastFill + repetitions), i] = 1 # ith index for ith letter of a-z
+        lastFill+=repetitions
+    # add space(s) at the end to mark word end
+    wordBins[lastFill:size, :] = 0
+    wordBins[lastFill:size, 26] = 1 # 26 denotes space
+    return wordBins
+
+
+'''
+    Find minimum repeating sequence in array of chars
+    Helper function for binmat2word
+'''
+def min_repetitions(charArr):
+    from collections import defaultdict
+    hist = defaultdict(int)
+    for ch in charArr:
+        hist[ch]+=1
+    return min(hist.values())
+
+
+'''
+    Reverse of word2binmat()
+'''
+def binmat2word(binmat):
+    codePoints = np.argmax(binmat, axis=1)
+    charArr = list(map(lambda x: chr(ord('a') + x) if 0 <= x <= 25 else '_', codePoints))
+    charArr = list(''.join(charArr).strip('_'))
+    word = '' # blank
+    size = len(charArr)
+    repetition = min_repetitions(charArr)
+    for i in np.arange(0,size, repetition):
+        word += charArr[i]
+
+    return word
---
+++ tests/text_processing_test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from modules.textprocessing import *
+
+def extract_timestamps_and_word_test():
+    """
+    >>> extract_timestamps_and_word('0 7 bin')
+    (0, 7, 'bin')
+    >>> extract_timestamps_and_word('26 38 again')
+    (26, 38, 'again')
+    >>> extract_timestamps_and_word('12.5 27.0 continue')
+    (12, 27, 'continue')
+    """
+
+def word2ints_test():
+    """
+    >>> word2ints('put')
+    array([15, 20, 19])
+    >>> word2ints('place')
+    array([15, 11,  0,  2,  4])
+    >>> word2ints('eight')
+    array([ 4,  8,  6,  7, 19])
+    """
+
+def word2binmat_test():
+    """
+    >>> np.argmax(word2binmat(6,7,'at'), axis=1)
+    array([ 0, 19])
+    >>> np.argmax(word2binmat(0,13,'foo'), axis=1)
+    array([ 5,  5,  5,  5, 14, 14, 14, 14, 14, 14, 14, 14, 26, 26])
+    >>> np.argmax(word2binmat(12,15,'four'), axis=1)
+    array([ 5, 14, 20, 17])
+    """
+
+def min_repetitions_test():
+    """
+    >>> min_repetitions(list('at'))
+    1
+    >>> min_repetitions(list('aaaaatttttt'))
+    5
+    >>> min_repetitions(list('fffooooooooo'))
+    3
+    """
+
+def binmat2word_test():
+    """
+    >>> binmat2word(word2binmat(0,5,'foo')) 
+    'foo'
+    >>> binmat2word(word2binmat(0,13,'seven'))
+    'seven'
+    >>> binmat2word(word2binmat(2,7,'oops'))
+    'oops'
+    """
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
