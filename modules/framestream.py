71df67b4336c6cc407b35f3011f7a86ed78cae8c
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2018-11-05 17:00:58 +0530

    add transcript stream for generating labels for keras

--- modules/framestream.py
+++ modules/framestream.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 import glob
 import os
-
+from .textprocessing import word2binmat, binmat2word, extract_timestamps_and_word
 
 class StreamInterface:
     def __init__(self):
@@ -94,3 +94,42 @@ class VideoStream(ImageStream):
     
     def __str__(self):
         return self.name + "\nsource = " + str(self.sourcePath) + "\nBuffer size:" + str(len(self.buffer))
+
+"""
+    Streams transcript words and converts transcript into binary matrix
+    See modules.textprocessing.word2binmat function description for details of binary representation
+"""
+class TranscriptFileStream(StreamInterface):
+
+    def __init__(self, sourcePath=None, timeFactor=1):
+        StreamInterface.__init__(self)
+        self.name = "TranscriptFileStream"
+        self.transcriptLines = []
+        self.timeFactor = timeFactor
+        if sourcePath != None:
+            self.set_source(sourcePath)
+    
+
+    def set_source(self, sourcePath):
+        self.sourcePath = sourcePath
+        with open(self.sourcePath, 'r') as f:
+            for line in f.readlines():
+                self.transcriptLines.append(line.strip())
+        
+    
+    def buffer_frames(self):
+        self.buffer = np.zeros((self.BUFFER_SIZE, 28))
+        for line in self.transcriptLines:
+            wordStart, wordEnd, word = extract_timestamps_and_word(line, self.timeFactor) 
+            self.buffer[wordStart: wordEnd + 1] = word2binmat(wordStart, wordEnd, word)
+        self.lastIndex = self.BUFFER_SIZE - 1
+    
+
+    def next_frame(self):
+        if len(self.buffer) == 0:
+            self.buffer_frames()
+        self.lastIndex-=1
+        if 0 < self.lastIndex < len(self.buffer):
+            return self.buffer[len(self.buffer) - self.lastIndex - 1]
+        else:
+            None
