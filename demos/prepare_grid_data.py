9d87432b7ef8e0eadb900a9119e72d3d88c5bd36
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2018-11-06 23:17:39 +0530

    show progess with tqdm

---
+++ demos/prepare_grid_data.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+
+from modules.framestream import VideoStream, TranscriptFileStream
+from modules.preprocessing import LipDetectorDlib
+import cv2 as cv
+import dlib
+import h5py
+from glob import glob
+import numpy as np
+from tqdm import tqdm
+
+out_img_width = 100
+out_img_height = 50
+
+dataset_path = sys.argv[1]
+sample_start = int(sys.argv[2])
+sample_size = int(sys.argv[3])
+sample_end = sample_start + sample_size - 1
+
+videos_path = 'videos'
+txt_path = 'transcripts'
+
+video_list = glob(os.path.join(dataset_path, videos_path) + '/*.mpg')[sample_start: sample_end+1]
+video_list.sort()
+
+sample_ids = list(map(lambda x: ''.join(os.path.basename(x).split('.')[:-1]), video_list))
+#print(sample_ids)
+
+lipDetector = LipDetectorDlib()
+lipDetector.model_from_file(os.path.join(os.path.abspath(
+        '..'), 'weights', 'shape_predictor_68_face_landmarks.dat'))
+
+
+X = np.zeros((sample_size, 75, out_img_height, out_img_width, 3))
+Y = np.zeros((sample_size, 75, 28))
+
+for i in tqdm(range(sample_size)):
+    transcript = []
+    vs = VideoStream()
+    ts = TranscriptFileStream(timeFactor=0.001)
+    #print(f"Processing file: {sample_ids[i]}")
+    try:
+        vs.sourcePath = video_list[i]
+        vs.set_source()
+        ts.set_source(os.path.join(dataset_path, txt_path, sample_ids[i]+'.align'))
+    except IndexError:
+        print('Index out of range')
+        sys.exit(1)
+    except IOError:
+        print(f'IOError when opening {sample_ids[i]}.align')
+    except Exception:
+        print(f'Error when processing {sample_ids[i]}')
+
+    vs.BUFFER_SIZE = 75
+
+    #print(transcript)
+
+    frame_no = 0
+
+    img = vs.next_frame()
+    label = ts.next_frame()
+    #win = dlib.image_window()
+    while img is not None:
+        bbox = lipDetector.get_bbox(img)
+        y1, x1, y2, x2 = bbox.left(), bbox.top(), bbox.right(), bbox.bottom()
+        lipImg = img[x1-3:x2+4, y1-3:y2+4,:]
+        
+        lipImg = cv.resize(lipImg, (out_img_width, out_img_height))
+        #win.set_image(lipImg)
+        X[i][frame_no] = lipImg
+        Y[i][frame_no] = label
+        img = vs.next_frame()
+        label = ts.next_frame()
+        frame_no+=1
+    
+
+with h5py.File(f"../datasets/grid_lips_{sample_start}-{sample_end}_100x50.hdf5", "w") as f:
+    f.create_dataset("features", data=X, dtype='i1', compression="gzip", compression_opts=4)
+    f.create_dataset("labels", (sample_size,75, 28), 'i1', Y, compression='gzip', compression_opts=4)
