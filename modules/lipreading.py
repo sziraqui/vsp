ec12b529054eba5f6fdfc460898aa9f587cfdfd6
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2018-11-08 05:14:31 +0530

    sentence reader initial

--- modules/lipreading.py
+++ modules/lipreading.py
@@ -94,29 +94,18 @@ class WordReader:
         return f"{self.name:\n}Model Summary:  {self.model.summary()}"
 
 
-class SentenceReader:
+class SentenceReader(WordReader):
 
-    def __init__(self):
-        self.name = "SentenceReader"
-        self.model = None
-        raise NotImplementedError(self.name + " constructor is not fully implemented")
-    
+   def __init__(self, name, params):
 
-    def create_model(self,params):
-        raise NotImplementedError(self.name + " create_model is not yet implemented")
-
-    
-    def train_model(self, trainParams):
-        raise NotImplementedError(self.name + " train_model is not yet implemented")
-
-    
-    def test_model(self, testParams):
-        raise NotImplementedError(self.name + " test_model is not yet implemented")
-
-
-    def predict_word(self):
-        raise NotImplementedError(self.name + " predict_word is not yet implemented")
-
+        WordReader.__init__(self)
+        self.frameLength = 75
+        self.frameWidth = 100
+        self.frameHeight = 50
+        if params['resume']:
+            self.model = model.load_model(params['model_file_checkpoint'])
+        else:
+            self.create_model(params)
 
     def __str__(self):
-        return self.name + ":\nModel = " + self.model
+        return f"{self.name:\n}Model Summary:  {self.model.summary()}"
