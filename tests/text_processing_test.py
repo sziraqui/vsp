f780734e4ba286fcae7ecc4de9f70d7bde621a08
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2018-12-02 22:07:29 +0530

    update tests

--- tests/text_processing_test.py
+++ tests/text_processing_test.py
@@ -20,11 +20,11 @@ def extract_timestamps_and_word_test():
 def word2ints_test():
     """
     >>> word2ints('put')
-    array([15, 20, 19])
+    array([15, 20, 19], dtype=int8)
     >>> word2ints('place')
-    array([15, 11,  0,  2,  4])
+    array([15, 11,  0,  2,  4], dtype=int8)
     >>> word2ints('eight')
-    array([ 4,  8,  6,  7, 19])
+    array([ 4,  8,  6,  7, 19], dtype=int8)
     """
 
 def word2binmat_test():
@@ -32,20 +32,11 @@ def word2binmat_test():
     >>> np.argmax(word2binmat(6,7,'at'), axis=1)
     array([ 0, 19])
     >>> np.argmax(word2binmat(0,13,'foo'), axis=1)
-    array([ 5,  5,  5,  5, 14, 14, 14, 14, 14, 14, 14, 14, 26, 26])
+    array([ 5,  5,  5, 14, 14, 14, 14, 27, 27, 14, 14, 14, 27, 27])
     >>> np.argmax(word2binmat(0,4,None), axis=1)
-    array([27, 27, 27, 27, 27])
+    array([26, 26, 26, 26, 26])
     """
 
-def min_repetitions_test():
-    """
-    >>> min_repetitions(list('at'))
-    1
-    >>> min_repetitions(list('aaaaatttttt'))
-    5
-    >>> min_repetitions(list('fffooooooooo'))
-    3
-    """
 
 def binmat2word_test():
     """
@@ -56,6 +47,39 @@ def binmat2word_test():
     >>> binmat2word(word2binmat(2,7,'oops'))
     'oops'
     """
+
+
+def wordExpansion_test():
+    """
+    >>> wordExpansion(0,5,'bin')
+    'biiiin'
+    >>> wordExpansion(5,17,'fallen')
+    'faaaal-leeeen'
+    >>> wordExpansion(9,15,'nine')
+    'niinee-'
+    >>> wordExpansion(0,0,'a')
+    'a'
+    >>> wordExpansion(0,3,'fall')
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "/home/sziraqui/Documents/vsp-dev/modules/textprocessing.py", line 108, in wordExpansion
+        assert timeLength >= len(newWord)
+    AssertionError
+    """
+
+
+def wordCollapse_test():
+    """
+    >>> wordCollapse('biiin')
+    'bin'
+    >>> wordCollapse('niinne-')
+    'nine'
+    >>> wordCollapse('_-pp-uuut--')
+    'put'
+    >>> wordCollapse('faaal-ll--enn--')
+    'fallen'
+    """
+
 
 if __name__ == '__main__':
     import doctest
