c555af2749a406e01dba83aac7043877bab5293b
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2018-12-18 23:22:34 +0530

    generator for ctc_loss ver of wordreader

--- modules/generators.py
+++ modules/generators.py
@@ -5,6 +5,8 @@ import numpy as np
 from keras.utils import normalize
 from .textprocessing import word2ints
 from .textprocessing import CODE_BLANK
+from .metrics import CTC_LOSS_STR
+
 
 class GeneratorInterface(object):
     def __init__(self):
@@ -70,3 +72,52 @@ class SimpleGenerator(GeneratorInterface):
         rstate.shuffle(X)
         rstate = np.random.RandomState(seed)
         rstate.shuffle(Y)
+
+
+class BatchForCTC(SimpleGenerator):
+
+    def next_batch(self, batchSize):
+        endIndex = 0
+        startIndex = endIndex
+        availableSamples = 0
+        np.random.shuffle(self.dataList)
+        while True:
+            X = np.zeros((self.batchSize, self.frameLength, self.frameHeigth, self.frameWidth, 3))
+            Y = np.zeros((self.batchSize, 32))
+            InpLen = np.zeros(self.batchSize, dtype='int32')
+            LabelLen = np.zeros(self.batchSize, dtype='int32')
+            try:
+                with h5py.File(self.dataList[self.dataIndex], 'r') as f:
+                    availableSamples = f["labels"][self.sampleIndex : self.sampleIndex + batchSize].shape[0]
+                    startIndex = endIndex%batchSize
+                    endIndex += availableSamples
+                    if  endIndex > batchSize:
+                        endIndex = batchSize
+                    X[startIndex:endIndex] = f["features"][self.sampleIndex : self.sampleIndex + endIndex - startIndex]
+                    Y[startIndex:endIndex] = f["labels"][self.sampleIndex : self.sampleIndex + endIndex - startIndex]
+                    InpLen[startIndex:endIndex] = f["input_length"][self.sampleIndex: self.sampleIndex + endIndex - startIndex]
+                    LabelLen[startIndex:endIndex] = f["label_length"][self.sampleIndex: self.sampleIndex + endIndex - startIndex]
+            except IOError as ioe:
+                self.dataIndex = (self.dataIndex + 1) % len(self.dataList)
+                self.sampleIndex = 0
+                endIndex = 0
+                print(repr(ioe))
+                continue
+            if endIndex >= batchSize:
+                endIndex = 0
+                self.sampleIndex += availableSamples
+                X = normalize(X, axis=1)
+                #self.shuffle_together(X, Y, self.seed)
+                inputs = {
+                    'input': X,
+                    'label_input': Y,
+                    'input_length': InpLen,
+                    'label_length': LabelLen
+                }
+                outputs = {CTC_LOSS_STR: np.zeros((self.batchSize, 32))}
+                yield inputs, outputs
+                del X
+                del Y
+            else:
+                self.dataIndex = (self.dataIndex + 1)%len(self.dataList)
+                self.sampleIndex = 0
