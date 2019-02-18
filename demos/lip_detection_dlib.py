1efe070467be055cb4f603247f47e4ff329ff0ac
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-02-18 17:56:22 +0530

    add bbox without dlib

--- demos/lip_detection_dlib.py
+++ demos/lip_detection_dlib.py
@@ -5,13 +5,14 @@ from modules.framestream import VideoStream
 from modules.preprocessing import LipDetectorDlib
 from skvideo.io import FFmpegWriter
 import dlib
+from modules.utils import Log, add_bbox, imshow
 
 vs = VideoStream()
 try:
     vs.sourcePath = sys.argv[1]
     vs.set_source()
 except IndexError:
-    print('No video source provided')
+    Log.error('No video source provided')
     sys.exit(1)
 
 lipDetector = LipDetectorDlib()
@@ -19,16 +20,15 @@ lipDetector.model_from_file(os.path.join(os.path.abspath('..'), 'weights','shape
 
 FPS = 25
 vs.BUFFER_SIZE = FPS * 3
-win = dlib.image_window()
+
 img = vs.next_frame()
 outfile = os.path.join(os.path.dirname(vs.sourcePath), 'out_' + os.path.basename(vs.sourcePath))
 out = FFmpegWriter(outfile)
 while img is not None:
-    win.set_image(img)
+
     bbox = lipDetector.get_bbox(img)
-    win.clear_overlay()
-    win.add_overlay(bbox)
-    #cv.rectangle(img, (bbox.left(), bbox.top()), (bbox.right(), bbox.bottom()), (255,0,0), 1)
+    img = add_bbox(img, bbox, (0.4, 0.6, 0.8))
     out.writeFrame(img)
     img = vs.next_frame()
+
 out.close()
--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,10 @@
 import logging
+import time
 from time import strftime
 from time import localtime
+from skimage.draw import polygon_perimeter, set_color
+import numpy as np
+import matplotlib.pyplot as plt
 
 def timeNow():
     return strftime('%Y-%M-%d %H:%m:%2ds', localtime())
@@ -17,3 +21,22 @@ def getLogger(className='', level=logging.INFO):
 
 # Global logger
 Log = getLogger()
+
+
+'''
+    image: ndarray as RGB floats
+    bbox: dlib.rectangle defining bounding box
+    color: tuple of (R,G,B) floats
+'''
+def add_bbox(image, bbox, color):
+    c = np.array([bbox.left(), bbox.left(), bbox.right(), bbox.right()], dtype=int)
+    r = np.array([bbox.bottom(), bbox.top(), bbox.top(), bbox.bottom()], dtype=int)
+    p = polygon_perimeter(r, c, shape=image.shape, clip=True)
+    img = image.copy()
+    set_color(img, p, color)
+    return img
+
+
+def imshow(image):
+    plt.imshow(image)
+    plt.show()
