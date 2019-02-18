519d9d925d8b9ccc74f0db597c7b59a40f4c5efb
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-02-18 13:04:30 +0530

    fix errors in webapp

--- demos/vsp_web_framework/Webapp.py
+++ demos/vsp_web_framework/Webapp.py
@@ -5,7 +5,7 @@ from werkzeug import secure_filename
 sys.path.insert(0, os.path.abspath('../../'))
 from modules.framestream import VideoStream, TranscriptFileStream
 from modules.preprocessing import LipDetectorDlib
-import cv2 as cv
+from modules.lipreading import SentenceReader
 import dlib
 from modules.textprocessing import *
 import h5py
@@ -25,9 +25,9 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def ok():
-   return render_template("VSP.html", data = None)
+   return render_template("VSP.html", data=None)
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods = ['POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
@@ -38,49 +38,36 @@ def upload_file():
             filename = secure_filename(f.filename)
             f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
             flash('File uploaded successfully!','success')
-            return redirect(url_for('ok'))
+            return render_template('VSP.html', data=None)
 
 
-
-   			
-
-
-
-
+@app.route('/process')
 def process():
     filelist = glob('uploads/*')
+    if len(filelist) == 0:
+        return render_template("VSP.html", data=None)
     path = filelist[0]
     X = lipdetector(path)
-    output = predict(X)
-    output_path='/static/output.txt'
-    file = open(output_path,'w')
-    file.write(output)
-    file.close()
-    return render_template("VSP.html",data = output)
+    output = predict(X[0])
+    return render_template("VSP.html", data = output)
 
 
 
 
 def lipdetector(path):
-    vs = VideoStream()
-    try:
-        vs.sourcePath = path
-        vs.set_source()
-    except IndexError:
-        print('No video source provided')
-        sys.exit(1)
+    out_img_heigth, out_img_width = 50, 100
+    vs = VideoStream(path)
 
     lipDetector = LipDetectorDlib()
-    lipDetector.model_from_file(os.path.join(os.path.abspath('..'), 'weights','shape_predictor_68_face_landmarks.dat'))
+    lipDetector.model_from_file(os.path.join(os.path.abspath('../..'), 'weights','shape_predictor_68_face_landmarks.dat'))
     frame_no = 0
     X = []
 
-    frames = np.zeros((75, out_img_height, out_img_width,3))
-    FPS = vs.stream.get(cv.CAP_PROP_FPS)
-    vs.BUFFER_SIZE = FPS * 5
+    frames = np.zeros((75, out_img_heigth, out_img_width,3))
+    FPS = 25
+    vs.BUFFER_SIZE = FPS * 3
     win = dlib.image_window()
     img = vs.next_frame()
-    out = cv.VideoWriter('out_' + os.path.basename(vs.sourcePath), cv.VideoWriter_fourcc('X','V','I','D'), 15, (480,360))
     while img is not None:
         win.set_image(img)
         bbox = lipDetector.get_bbox(img)
@@ -88,15 +75,19 @@ def lipdetector(path):
         y1, x1, y2, x2 = bbox.left(), bbox.top(), bbox.right(), bbox.bottom()
         lipImg = img[x1-3:x2+4, y1-3:y2+4,:]
         
-        lipImg = cv.resize(lipImg, (out_img_width, out_img_height))
-        #win.set_image(lipImg)
+        lipImg = VideoStream.scale_source(lipImg, (out_img_heigth, out_img_width))
+        
         frames[frame_no] = lipImg
         img = vs.next_frame()
         frame_no+=1
+        if frame_no == vs.BUFFER_SIZE:
+            break
    
         img = vs.next_frame()
     X.append(frames)
-    return X   
+    X = np.array(X)
+    print(X.shape)
+    return X
     
 def predict(frames):
     params = {
@@ -118,20 +109,13 @@ def predict(frames):
         "validation_split": 0.2,
         "log_dir": "../logs"
     }    
-    params["model_file"] = "../weights/lipnet_ctc_s{0}_b{1}_e{2}_{3}.hdf5".format(params['sample_size'], params['batch_size'], params['epochs'], params['epochs'])
+    params["model_file"] = "../../weights/lipnet_ctc_s{0}_b{1}_e{2}_{3}.hdf5".format(params['sample_size'], params['batch_size'], params['epochs'], params['epochs'])
     params['sample_size'] = 64
     params['batch_size'] = 32
     params['epochs'] = 1
-    params['model_file_checkpoint'] = "../weights/lipnet_ctc_s128_b32_e300_18-12-2018-19-02-24.hdf5"
+    params['model_file_checkpoint'] = "../../weights/lipnet_ctc_s128_b32_e300_18-12-2018-19-02-24.hdf5"
     sr = SentenceReader(params)
-    generator = BatchForCTC(params)
-    X,Y, il,ll = tuple([None]*4)
-    samples = 32
-    for i,o in generator.next_batch(1):
-        X,Y,il,ll = i['input'], i['label_input'], i['input_length'], i['label_length']
-        break
-    print(X.shape)
-    print(Y.shape)
+    
     output = sr.predict_sentence(frames)
     return output
 
